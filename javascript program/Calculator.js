676b2dc76920a68ca86e356bab7ff3ab703a6f8f
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-04 16:08:09 +0530

    Programs Updated

--- javascript program/Calculator.js
+++ javascript program/Calculator.js
@@ -1,6 +1,17 @@
-a = 10;
-b = 15;
+let a = 10;
+let b = 15;
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+// This are some basic operations in javascript
+
+var x = 2;
+let y = 22;
+const z = 222;
+z = 45; // I cant change the value of z as it is specified constant
+console.log(x);
+console.log(y);
+console.log(z);
