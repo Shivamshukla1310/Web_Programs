2f56989c359e5d8dd0447325dd85ba0ae46d7d17
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-04 16:23:03 +0530

    Programs Updated

---
+++ javascript program/Operators.js
@@ -0,0 +1,10 @@
+// This is just to understand operators
+let a = 12;
+let b = 20;
+let c = 3;
+console.log(a == b);
+console.log(a === b);
+console.log(a > b);
+console.log(a < b);
+console.log(a > b && b > c);
+console.log(a > b || a > c);
