0494b878e8388d2e507d4988442ded6d35b56a50
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-13 22:26:27 +0530

    Programs Updated

--- javascript program/reverse_string.js
+++ javascript program/reverse_string.js
@@ -1,9 +1,21 @@
 // This program is to reverse the given string
 
+// function reverseString(str) {
+//   for (let i = str.length; i >= 0; i--) {
+//     console.log(str[i] + " ");
+//   }
+// }
+
+// reverseString("Hello");
+
+// Well the above approach prints the string in new line so to overcome that thing we can use another method
+
 function reverseString(str) {
-  for (let i = str.length; i >= 0; i--) {
-    console.log(str[i] + " ");
+  let rev = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev += str[i];
   }
+  console.log(rev);
 }
 
 reverseString("Hello");
