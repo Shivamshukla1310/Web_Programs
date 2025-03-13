bde74954e641d2f8afddd49ae121e2d797db0faf
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-13 22:21:21 +0530

    Programs of reverse string updated

---
+++ javascript program/reverse_string.js
@@ -0,0 +1,9 @@
+// This program is to reverse the given string
+
+function reverseString(str) {
+  for (let i = str.length; i >= 0; i--) {
+    console.log(str[i] + " ");
+  }
+}
+
+reverseString("Hello");
