3bb22398286ea98ba3b82130d0569d48d3e6a36e
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-28 23:33:33 +0530

    Number Checker done

---
+++ javascript program/variable_check.js
@@ -0,0 +1,10 @@
+// this program checks weather the varible is number or not
+
+let num = '12';
+
+if(typeof num === "number"){
+    console.log("Its number");
+}
+else{
+    console.log("Its Not Number");
+}
