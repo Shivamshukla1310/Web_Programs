4b635006d11dbbee25f67d4c67840bdda6c15f44
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-28 23:17:57 +0530

    Programs Done

---
+++ javascript program/check_String.js
@@ -0,0 +1,8 @@
+// Check weather the first string is equal to second one
+
+let String1 = "1";
+let String2 = 1;
+
+let Check = String1 == String2 ? "Its Equal" : "Not Equal";
+
+console.log(Check);
