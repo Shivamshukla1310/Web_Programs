3bc07f9f46ff13c8d5684122f9f8ff1c33471cc8
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-29 08:45:45 +0530

    Copy Of Tea Done

---
+++ javascript program/array_challenge_6.js
@@ -0,0 +1,8 @@
+/* 
+6. You have an array named `popularTeas` containing `"green tea"`, `"oolong tea"`, and `"chai"`. 
+   Create a soft copy of this array named `softCopyTeas`.
+*/
+
+let popularTeas = ["green tea", "oolong tea", "chai"];
+let copyOfOgTea = popularTeas
+console.log(`${popularTeas} \n ${copyOfOgTea}`);
