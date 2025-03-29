91b3ffec7dd46fb983995b3488b5813bc74ca48e
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-29 08:59:09 +0530

    Finding array length program done

--- javascript program/array_challenge_9.js
+++ javascript program/array_challenge_9.js
@@ -2,3 +2,6 @@
 9. You have an array named `teaMenu` containing `"masala chai"`, `"oolong tea"`, `"green tea"`, and `"earl grey"`. 
    Find the length of the array and store it in a variable named `menuLength`.
 */
+
+let teaMenu = ["masala chai", "oolong tea", "earl grey"];
+console.log(teaMenu.length);
