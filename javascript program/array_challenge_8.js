d043d2adc9a0685bda715f20632b05d1d1d99c33
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-29 08:55:49 +0530

    Concatenantion done

---
+++ javascript program/array_challenge_8.js
@@ -0,0 +1,11 @@
+/* 
+8. You have two arrays: `europeanCities` containing `"Paris"` and `"Rome"`, and `asianCities` containing `"Tokyo"` and `"Bangkok"`. 
+   Merge these two arrays into a new array named `worldCities`.
+*/
+
+let europeanCities = ["Paris", "Rome"];
+let asianCities = ["Tokyo", "Bangkok"];
+let worldCities = europeanCities.concat(asianCities);
+console.log(worldCities)
+let newWorldCities = europeanCities + asianCities;
+console.log(newWorldCities);
---
+++ javascript program/array_challenge_9.js
@@ -0,0 +1,4 @@
+/* 
+9. You have an array named `teaMenu` containing `"masala chai"`, `"oolong tea"`, `"green tea"`, and `"earl grey"`. 
+   Find the length of the array and store it in a variable named `menuLength`.
+*/
