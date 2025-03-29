5e2a0bb35a4d5311371b8bfc6d6c466f858ddc1a
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-29 09:01:59 +0530

    All Challenges Completed

---
+++ javascript program/array_challenge_10.js
@@ -0,0 +1,10 @@
+/* 
+10. You have an array named `cityBucketList` containing `"Kyoto"`, `"London"`, `"Cape Town"`, and `"Vancouver"`. 
+    Check if `"London"` is in the array and store the result in a variable named `isLondonInList`.
+*/
+
+let cityBucketList = ["Kyoto", "London", "Cape Town", "Vsncouver"];
+
+let check = cityBucketList.includes("London");
+
+console.log(check);
