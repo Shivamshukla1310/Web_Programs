89c15bec57a15caddafe6e81f65a43b595a34790
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-29 08:36:33 +0530

    To change the 2nd element done

---
+++ javascript program/array_challenge_3.js
@@ -0,0 +1,8 @@
+/* 
+3. You have an array named `teaTypes` containing `"herbal tea"`, `"white tea"`, and `"masala chai"`. 
+   Change the second element of the array to `"jasmine tea"`.
+*/
+
+let teaTypes = ["herbal", "white", "masala"];
+teaTypes[1] = "jasmine"
+console.log(teaTypes)
