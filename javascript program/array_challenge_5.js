389a82d6f29fbc702389b7104f41c8a501ae735e
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-29 08:41:11 +0530

    Removing Challenge Done

---
+++ javascript program/array_challenge_5.js
@@ -0,0 +1,8 @@
+/* 
+5. You have an array named `teaOrders` with `"chai"`, `"iced tea"`, `"matcha"`, and `"earl grey"`. 
+   Remove the last element of the array using the `pop` method and store it in a variable named `lastOrder`.
+*/
+
+let teaOrders = ["chai", "iced", "matcha", "earl grey"];
+teaOrders.pop("earl grey");
+console.log(teaOrders);
